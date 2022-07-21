78f140fa4f11d88924b61707330ef2ef83bb2cce
Juan Cordoba <juancordoba75@gmail.com>
2022-07-21 17:56:16 -0300

    borre todo

--- back/src/api/routes/Articulos.routes.ts
+++
@@ -1,9 +0,0 @@
-const {Router} = require('express');
-const router = Router();
-
-import { findAll } from '../controllers/Articulos.controller'
-
-router.route('/')
-.get(findAll)
-
-export default router
