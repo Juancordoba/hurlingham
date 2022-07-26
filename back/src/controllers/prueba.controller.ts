319e8ac5eb4997b5907564982918fe3e31cc32f5
Juan Cordoba <juancordoba75@gmail.com>
2022-07-26 16:14:46 -0300

    inicio back

---
+++ back/src/controllers/prueba.controller.ts
@@ -0,0 +1,46 @@
+// Uncomment these imports to begin using these cool features!
+
+import {inject} from '@loopback/core';
+import {
+  get, Request, response, ResponseObject, RestBindings
+} from '@loopback/rest';
+
+const PRUEBA_RESPONSE: ResponseObject = {
+  description: 'Ping Response',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        title: 'PingResponse',
+        properties: {
+          greeting: {type: 'string'},
+          date: {type: 'string'},
+          url: {type: 'string'},
+          headers: {
+            type: 'object',
+            properties: {
+              'Content-Type': {type: 'string'},
+            },
+            additionalProperties: true,
+          },
+        },
+      },
+    },
+  },
+};
+
+export class PruebaController {
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) { }
+
+  @get('/prueba')
+  @response(200, PRUEBA_RESPONSE)
+  prueba(): object {
+    // Reply with a greeting, the current time, the url, and request headers
+    return {
+      greeting: 'Hello from LoopBack',
+      date: new Date(),
+      url: this.req.url,
+      headers: Object.assign({}, this.req.headers),
+    };
+  }
+}
