1bad2ad7d956e9608e64009099773211d87d5d41
Juan Cordoba <juancordoba75@gmail.com>
2022-07-28 20:23:16 -0300

    prueba back

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginButton from './components/login'
+
+function App() {
+  return (
+    <div className="App">
+      <LoginButton />
+    </div>
+  );
+}
+
+export default App;
